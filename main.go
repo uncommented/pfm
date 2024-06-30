080a56ba58856538369d232111a590dfab92898f
Jiseong Bak <jiseongg301@gmail.com>
2024-06-30 19:03:47 +0900

    refactor: tidy console output

--- main.go
+++ main.go
@@ -28,21 +28,35 @@ func main() {
 	if vendor == "kis" {
 		jsonRes := kis.RequestBalance()
 
-		output1 := jsonRes["output1"].([]interface{})[0]
+		balancesPerItem := jsonRes["output1"].([]interface{})
+		overallPerformance := jsonRes["output2"].(map[string]interface{})
 		clear(jsonRes)
+		fmt.Println("\n=== Balance ===")
+		for i, _balance := range balancesPerItem {
+			balance := _balance.(map[string]interface{})
+			itemName := balance["ovrs_item_name"]
+			ticker := balance["ovrs_pdno"]
+			fmt.Printf("%d. %s (%s)\n", i+1, itemName, ticker)
+			fmt.Printf(" Holdings: %s\n", balance["ord_psbl_qty"])
+			fmt.Printf(" Avg. purchasing price: %s\n", balance["pchs_avg_pric"])
+			fmt.Printf(" Current price: %s\n", balance["now_pric2"])
+			fmt.Printf(" Profit(%%): %s\n\n", balance["evlu_pfls_rt"])
+		}
 
-		fmt.Printf("Item: %s\n", output1.(map[string]interface{})["ovrs_item_name"])
-		fmt.Printf("Holdings: %s\n", output1.(map[string]interface{})["ord_psbl_qty"])
-		fmt.Printf("Avg. purchasing price: %s\n", output1.(map[string]interface{})["pchs_avg_pric"])
-		fmt.Printf("Current price: %s\n", output1.(map[string]interface{})["now_pric2"])
-		fmt.Printf("Profit(%%): %s\n", output1.(map[string]interface{})["evlu_pfls_rt"])
+		fmt.Println("\n=== Performance ===")
+		fmt.Printf("Purchasing amount: %s\n", overallPerformance["frcr_pchs_amt1"])
+		fmt.Printf("Evaluation amount: %s\n", overallPerformance["tot_evlu_pfls_amt"])
+		fmt.Printf("Realized profit/loss ($): %s\n", overallPerformance["ovrs_rlzt_pfls_amt"])
+		fmt.Printf("Realized profit/loss (%%): %s\n", overallPerformance["rlzt_erng_rt"])
+		fmt.Printf("Total profit ($): %s\n", overallPerformance["ovrs_tot_pfls"])
+		fmt.Printf("Total profit (%%): %s\n\n", overallPerformance["tot_pftrt"])
 	} else {
 		jsonRes := upbit.RequestBalance()
-		for _, currency := range jsonRes {
-			fmt.Println("---")
-			fmt.Printf("Currency: %s\n", currency["currency"].(string))
-			fmt.Printf("Balance: %s\n", currency["balance"].(string))
-			fmt.Printf("Avg. purchasing price: %s\n", currency["avg_buy_price"].(string))
+		for i, currency := range jsonRes {
+			currencyName := currency["currency"]
+			fmt.Printf("%d. %s\n", i+1, currencyName)
+			fmt.Printf(" Balance: %s\n", currency["balance"].(string))
+			fmt.Printf(" Avg. purchasing price: %s\n\n", currency["avg_buy_price"].(string))
 		}
 		clear(jsonRes)
 	}
