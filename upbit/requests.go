0b45cbe62d8942d21d4038ae032cbd9c0587c4b0
Jiseong Bak <jiseongg301@uncom.dev>
2024-07-04 23:58:24 +0900

    refactor(upbit): merge files for rest api call

--- upbit/requests.go
+++ upbit/requests.go
@@ -1,14 +1,47 @@
 package upbit
 
 import (
+	"crypto/sha512"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 const UPBIT_URL = "https://api.upbit.com"
+
+func RequestToken(query string) (string, error) {
+	var token *jwt.Token
+
+	accesskey := os.Getenv("UPBIT_ACCESS_KEY")
+	secretkey := os.Getenv("UPBIT_SECRET_KEY")
+
+	if query == "" {
+		token = jwt.NewWithClaims(jwt.SigningMethodHS256,
+			jwt.MapClaims{
+				"access_key": accesskey,
+				"nonce":      uuid.New().String(),
+			})
+	} else {
+		sha_512 := sha512.New()
+		sha_512.Write([]byte(query))
+		query_hash := fmt.Sprintf("%x", sha_512.Sum(nil))
+		token = jwt.NewWithClaims(jwt.SigningMethodHS256,
+			jwt.MapClaims{
+				"access_key":     accesskey,
+				"nonce":          uuid.New().String(),
+				"query_hash":     query_hash,
+				"query_hash_alg": "SHA512",
+			})
+	}
+
+	return token.SignedString([]byte(secretkey))
+}
 
 func RequestBalance() []map[string]interface{} {
 	req, err := http.NewRequest("GET", UPBIT_URL+"/v1/accounts", nil)
